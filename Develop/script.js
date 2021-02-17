964c1b6e01c89de4e843ca5faeb475e244676d44
Javiistacks <javiann.marrero@icloud.com>
2021-02-17 14:54:08 -0600

    added special, lower and uppercase character

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,55 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword(
+  passwordLenth,
+  includeSpecial,
+  includeLowercase,
+  includeUppercase,
+  ) {
+
+    let characterOptions=[]
+
+    let lowercase= ["a","b","c","d"];
+    let uppercase= ["A","B","C","D"];
+    let special= ["!","@","#","$"];
+    
+    if (includeSpecial){
+      characterOptions= characterOptions.concat(special);
+
+    }
+
+    if (uppercase){
+      characterOptions= characterOptions.concat(uppercase);
+
+    }
+
+    if (lowercase){
+      characterOptions= characterOptions.concat(lowercase);
+
+    }
+    
+    
+    let res= ""
+  
+      for (var i = 0; i <passwordLenth; i++) {
+        let randomIndex = Math.floor(Math.random()) *characterOptions
+          res += characterOptions[randomIndex]
+
+    }
+      
+      
+      return res
+
+}
 // Write password to the #password input
 function writePassword() {
+      let passwordLenth =10;
+      let includeSpecial=true;
+      let includeLowercase=true;
+      let includeUppercase=false;
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
